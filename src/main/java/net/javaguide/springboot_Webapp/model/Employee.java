ad5e0cb12e1855f20a1848e450dac2b8390707fd
Gaurav3002 <gaurav.kumar141312@gmail.com>
2025-04-05 20:03:17 +0530

    update version3

--- src/main/java/net/javaguide/springboot_Webapp/model/Employee.java
+++ src/main/java/net/javaguide/springboot_Webapp/model/Employee.java
@@ -3,9 +3,9 @@ package net.javaguide.springboot_Webapp.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
